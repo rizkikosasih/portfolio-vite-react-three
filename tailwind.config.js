5fa742ad36eb57760c3a312e4b46bcc72ec26b00
Rizki 2 <rizkigames9@gmail.com>
2024-01-16 08:06:55 +0700

    update screen size

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,6 +26,13 @@ export default {
         card: '0px 1px 2px 0px rgba(0, 0, 0, 0.05)'
       }
     },
+    screens: {
+      'sm': '480px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
   },
   plugins: [],
 }
